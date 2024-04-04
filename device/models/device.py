107727cad474ece85734ff2f9e720371c46265b7
MY PC <khaquyen317@gmail.com>
2024-04-04 14:44:52 +0700

    device

---
+++ device/models/device.py
@@ -0,0 +1,29 @@
+from odoo import fields, models, api
+
+
+class Device(models.Model):
+    _name = 'device.device'
+    _description = 'Thiết bị'
+
+    date_import = fields.Date('Ngày nhập hàng', default=fields.Date.today(), track_visibility='onchange')
+    first_date_use = fields.Date('Ngày đưa vào sử dụng', default=fields.Date.today(), track_visibility='onchange')
+    period = fields.Integer("Bảo hành(Tháng)")
+    description = fields.Html("mô tả")
+    status = fields.Selection(
+        [('not_used', 'Chưa sử dụng'), ('using', 'Đang sử dụng'), ('out_of_warranty', 'Hết bảo hành'),
+         ('liquidate', 'Chờ thanh lý'), ('is_broken', 'Đang hỏng'), ('loss', 'Bị mất')], string='Trạng thái',
+        required=True, default='not_used', track_visibility="onchange")
+
+
+class DeviceImage(models.Model):
+    _name = 'device.image'
+    _description = 'Image device'
+    name = fields.Char('name')
+    image = fields.Binary('Image')
+    description = fields.Text('Mô tả')
+    main_device_id = fields.Many2one('device.main')
+
+
+# class Attachment(models.Model):
+#     _inherit = 'ir.attachment'
+#     date = fields.Date('Attachment date', default=lambda self: fields.Date.today())
---
+++ device/models/device_extra.py
@@ -0,0 +1,36 @@
+import base64
+
+from odoo import fields, models, api
+from odoo.modules import get_module_resource
+
+
+class ModelName(models.Model):
+    _name = 'device.extra'
+    _inherit = 'device.device'
+    _description = 'Device extra'
+
+    @api.model
+    def _default_image(self):
+        image_path = get_module_resource('device', 'static/img', 'default_image.png')
+        return base64.b64encode(open(image_path, 'rb').read())
+
+    name = fields.Char('Tên thiết bị', required = True)
+    code = fields.Char('Mã thiết bị', required= True)
+    device_main_id = fields.Many2one('device.main','Thiết bị chính')
+    amount = fields.Integer('Số lượng', default= 1)
+    price = fields.Integer('Giá', default=0)
+    partner = fields.Char('Nhà cung cấp')
+    image_1920 = fields.Binary(default=_default_image, store=True)
+
+    def open_liquidate(self):
+        self.status = 'liquidate'
+
+    def open_is_broken(self):
+        self.status = 'is_broken'
+
+    def open_loss(self):
+        self.status = 'loss'
+
+    def open_using(self):
+        self.status = 'using'
+
